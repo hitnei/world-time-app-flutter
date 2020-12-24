ea8d9df9a289bbb4d158f4a0bf7b6705c831df08
hitnei <hoangnhi050298@gmail.com>
2020-12-24 14:33:02 +0700

    initial

---
+++ android/app/src/main/kotlin/WorldTime/worldtime/MainActivity.kt
@@ -0,0 +1,6 @@
+package WorldTime.worldtime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+      home: Home()
+  )
+);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('World Time'),
+      ),
+    );
+  }
+}
